a343b79eed9c61fa0b30e79c7e7d489642ba8384
voyager14 <21mh124@queensu.ca>
2026-09-10 12:02:40 -0400

    add speed and analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
@@ -9,5 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
